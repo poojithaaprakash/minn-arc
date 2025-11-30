18b86dad1743195cc4953488b802ba4c5c56313e
poojithaaprakash <poojithaaprakash@gmail.com>
2025-11-30 16:29:53 +0530

    redirect url for order now

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/minnarc-02.png" type="image/png" />
+      </head>
       <body>
         <Navbar />
         <main className="min-h-screen">
